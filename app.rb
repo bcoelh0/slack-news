fe0a66db4cae4c4798dab2e79a61252fec6c0a10
Bruno Coelho <bruno@charliehr.com>
2020-04-08 19:10:28 +0100

    Init app!

---
+++ app.rb
@@ -0,0 +1,24 @@
+require 'sinatra'
+
+get '/' do
+  'Hello world!'
+end
+
+get '/auth' do
+  client_id = '1052655198181.1050480329478'
+  scope = 'chat:write:bot'
+  redirect_uri = 'http://localhost:4567/auth_code'
+  redirect "https://slack.com/oauth/authorize?client_id=#{client_id}&scope=#{scope}&redirect_uri=#{redirect_uri}"
+end
+
+get '/auth_code' do
+  client_id = '1052655198181.1050480329478'
+  client_secret = '886859a42adcabce467cf04375d63f97'
+  code = params['code']
+  redirect_uri = 'http://localhost:4567/auth_success'
+  redirect "https://slack.com/api/oauth.access?client_id=#{client_id}&client_secret=#{client_secret}&code=#{code}&redirect_uri=#{redirect_uri}"
+end
+
+get '/auth_success' do
+  "All done!"
+end
